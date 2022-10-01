395f1f46b008067e4f6a1f187f13201b9833e992
s3mons <97874282+s3mons@users.noreply.github.com>
2022-10-02 02:35:03 +0700

    kinemetic_server

---
+++ lab2/dummy_kinematics/dummy_kinematics/forward_kinematics.py
@@ -0,0 +1,59 @@
+import numpy as np
+import math
+def forward_kinematics(q):
+    joint_config = q
+    n_joint = 3
+    DH_pam = [[0 ,0 ,0.3 ,0],
+              [0.16 ,1.57 ,0 ,0],
+              [0.35 ,0 ,0 ,0]]
+
+    H = np.identity(4)
+
+    H_end_eff = [[1 ,0 ,0 ,0.16],
+                 [0 ,0 ,1 ,0],
+                 [0 ,-1 ,0 ,0],
+                 [0 ,0 ,0 ,1]]
+
+    for i in range(n_joint):
+        rad = joint_config[i] * math.pi/180
+        c ,s = np.cos(rad), np.sin(rad)
+        c_t ,s_t = np.cos(0), np.sin(0)
+        c_x ,s_x = np.cos(DH_pam[i][1]), np.sin(DH_pam[i][1])
+        c_z ,s_z = np.cos(DH_pam[i][3]), np.sin(DH_pam[i][3])
+
+        T_x =  [[c_t ,-s_t ,0 ,DH_pam[i][0]],
+                [s_t , c_t ,0 ,0],
+                [0 , 0 ,1 ,0],
+                [0 , 0 ,0 ,1]]
+
+        R_x =  [[c_x ,-s_x ,0 ,0],
+                [s_x , c_x ,0 ,0],
+                [0 , 0 ,1 ,0],
+                [0 , 0 ,0 ,1]]
+
+        T_z =  [[c_t ,-s_t ,0 ,DH_pam[i][2]],
+                [s_t , c_t ,0 ,0],
+                [0 , 0 ,1 ,0],
+                [0 , 0 ,0 ,1]]
+
+        R_z =  [[c_z ,-s_z ,0 ,0],
+                [s_z , c_z ,0 ,0],
+                [0 , 0 ,1 ,0],
+                [0 , 0 ,0 ,1]]
+
+        H_j =  [[c ,-s ,0 ,0],
+                [s , c ,0 ,0],
+                [0 , 0 ,1 ,0],
+                [0 , 0 ,0 ,1]]
+
+        H = H @ T_x @ R_x @ T_z @ R_z @ H_j      
+        # H = np.matmul(H ,T_x) 
+        # H = np.matmul(H ,R_x)  
+        # H = np.matmul(H ,T_z)  
+        # H = np.matmul(H ,R_z)  
+        # H = np.matmul(H ,H_j) 
+        # print(H)
+        # print(HH)
+
+    return np.matmul(H,H_end_eff)
+# print(forward_kinematics([30.0,45,0]))
--- lab2/dummy_kinematics/scripts/kinematics_server.py
+++ lab2/dummy_kinematics/scripts/kinematics_server.py
@@ -2,16 +2,20 @@
 
 # import all other neccesary libraries here
 from tokenize import String
+from turtle import forward
 import numpy as np
+import math
 from argparse import Namespace
 from platform import node
 import sys
 
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import JointState
 from dummy_kinematics_interfaces.srv import SetJoint
-# from std_msgs.msg import Float64
-
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Point
+from dummy_kinematics.forward_kinematics import forward_kinematics
 
 class Kinematics_Server(Node):
 
@@ -25,44 +29,53 @@ class Kinematics_Server(Node):
         super().__init__('kinematics_server')
 
         # Create Service 
-        self.joint_state = self.create_service(SetJoint,'set_joint',self.set_joint_callback)
+        self.joint_states = self.create_service(SetJoint,'set_joint',self.set_joint_callback)
 
         # Create Publisher
-        # self.command_publisher = self.create_publisher(Float64,'noise',10)
-
+        self.command_publisher = self.create_publisher(Point,'joint_states',10)
+        
         # Publish
         timer_period = 1/self.rate
         self.timer = self.create_timer(timer_period,self.timer_callback)
 
-
         # additional attributes
-        self.mean = 0.0
-        self.variance = 1.0
-        self.get_logger().info(f'Starting {self.get_namespace()}/{self.get_name()} with the default parameter. mean: {self.mean}, variance: {self.variance}')
-    
+        self.joint_state_position = JointState()
+        self.joint_state_position.name = ['Joint1' ,'Joint2' ,'Joint3']
+        self.joint_state_position.position = [0. , 0. , 0.]
+
+
     def set_joint_callback(self,request:SetJoint.Request,response:SetJoint.Response):
 
-        self.mean = request.setjoints
-        self.variance = request.variance.data**(1/2)
-        # self.get_logger().info('Incoming request\nmean: %d variance: %d' % (request.mean, request.variance))
-  
+        self.joint_state_position.position[0] = request.q1.data
+        self.joint_state_position.position[1] = request.q2.data
+        self.joint_state_position.position[2] = request.q3.data
+
+        fk = forward_kinematics(self.joint_state_position.position)
+        response.x.data = fk[0][3] 
+        response.y.data = fk[1][3] 
+        response.z.data = fk[2][3] 
+
         return response
     
     def timer_callback(self):
-        # remove pass and add codes here
-        msg = Float64()
-        msg.data = np.random.normal(self.mean, self.variance)
+
+        msg = Point()
+        msg.x = self.joint_state_position.position[0]
+        msg.y = self.joint_state_position.position[1]
+        msg.z = self.joint_state_position.position[2]
+
         self.command_publisher.publish(msg)
-        print(self.rate)
-        
+        # print(self.rate)
+
+    
 
 def main(args=None):
     # remove pass and add codes here
     rclpy.init(args=args)
-    noise_gen = NoiseGenerator()
+    joint_state = Kinematics_Server()
     print("Start")
-    rclpy.spin(noise_gen)
-    noise_gen.destroy_node()
+    rclpy.spin(joint_state)
+    joint_state.destroy_node()
     rclpy.shutdown()
 
 if __name__=='__main__':
